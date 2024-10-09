7ac2ae294c07f98287789dcadf77c3cf2fa62ef5
James Mitofsky <JamesMitofsky@gmail.com>
2024-10-09 08:21:38 +0200

    Add redirect to strava

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,11 @@ const nextConfig = {
     ],
   },
   redirects: async () => {
-    return [];
+    return [  {
+        source: '/strava',
+        destination: 'https://www.strava.com/athletes/18429685',
+        permanent: false, // Set to true for a 308 permanent redirect, false for a 307 temporary redirect
+      },];
   },
 };
 
