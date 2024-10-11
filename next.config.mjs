98ea9574d97911da6b44a5a7d5ddc15a81c828cd
James Mitofsky <JamesMitofsky@gmail.com>
2024-10-11 14:53:02 +0200

    Add linkedin as a redirect

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,13 @@ const nextConfig = {
         source: '/strava',
         destination: 'https://www.strava.com/athletes/18429685',
         permanent: false, // Set to true for a 308 permanent redirect, false for a 307 temporary redirect
-      },];
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/jamesmit',
+        permanent: false, // Set to true for a 308 permanent redirect, false for a 307 temporary redirect
+      },
+    ];
   },
 };
 
