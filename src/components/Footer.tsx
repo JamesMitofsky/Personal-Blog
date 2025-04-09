ddd5d00f783c6bcd16d4c9e33ef4958a239dfd5a
James Mitofsky <jamesmitofsky@gmail.com>
2025-04-09 12:55:38 +0200

    Add footer with my name to improve SEO for James Mitofsky

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,22 +4,7 @@ import { FunctionComponent } from "react";
 export const Footer: FunctionComponent = () => {
   return (
     <section className="mb-10 md:mt-24 md:mb-10 flex justify-center items-center [min-h-screen:mt-auto]">
-    {/* <section className="mt-10 mb-4 md:mt-24 md:mb-10 flex justify-center items-center [min-h-screen:mt-auto]"> */}
-
-      {/* <a
-        target="_blank"
-        href="https://btv.dev"
-        className="font-normal text-gray-500 text-sm flex items-center"
-      >
-        Made by <img
-          src="/BTV.svg"
-          alt="BTV dot Dev logo"
-          width={60}
-          height={20}
-          className="w-[80px] h-auto -mb-[2px] ml-[2px]"
-        />
-      </a> */}
-
+      Coded & written by James Mitofsky
     </section>
   );
 };
